2252b6ff039ddd75935e3f75d0f20b5fe9389af1
ismartkorea <ismartkorea01@gmail.com>
2023-06-26 23:47:24 +0900

    수정

--- src/main/java/com/products/service/ProductsServiceImpl.java
+++ src/main/java/com/products/service/ProductsServiceImpl.java
@@ -11,7 +11,7 @@ import com.products.model.ProductModel;
 
 @Service("productsService")
 public class ProductsServiceImpl implements ProductsService {
-	@Autowired ProductsDao proDao;
+	@Autowired ProductsDao productsDao;
 	
 	@Transactional
 	public int addAllProduct(List<ProductModel> pdList) {
@@ -20,28 +20,28 @@ public class ProductsServiceImpl implements ProductsService {
 		
 		for(ProductModel product : pdList){
 			if(product.getState().equals("created"))
-				num += proDao.addProduct(product);
+				num += productsDao.addProduct(product);
 			else if(product.getState().equals("updated"))
-				num += proDao.updateProduct(product);
+				num += productsDao.updateProduct(product);
 			else if(product.getState().equals("deleted"))
-				num += proDao.delProduct(product.getCode());
+				num += productsDao.delProduct(product.getCode());
 		}
 		return num;
 	}
 	
 	public List<ProductModel> getProductList() {
-		return proDao.getProductList();
+		return productsDao.getProductList();
 	}
 
 	public int addProduct(ProductModel product) {
-		return proDao.addProduct(product);
+		return productsDao.addProduct(product);
 	}
 
 	public int updateProduct(ProductModel product) {
-		return proDao.updateProduct(product);
+		return productsDao.updateProduct(product);
 	}
 
 	public int delProduct(String code) {
-		return proDao.delProduct(code);
+		return productsDao.delProduct(code);
 	}
 }
