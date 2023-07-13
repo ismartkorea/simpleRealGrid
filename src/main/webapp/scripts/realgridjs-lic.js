ee357819f694f89e2788928fb95d33267469c881
ismartkorea <ismartkorea01@gmail.com>
2023-07-13 22:20:14 +0900

    - realgrid 개발라이센스연장, 테스트용 Controller 추가.

--- src/main/webapp/scripts/realgridjs-lic.js
+++ src/main/webapp/scripts/realgridjs-lic.js
@@ -1,1 +1,1 @@
-var realGridJsLic = 'upVcPE+wPOksRHztag2HLt+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0adoq8hudFeXNTakk0yR9Uc3dEA1pRRhIdEHiG7csiBBkxValpjT3ck=';
+var realGridJsLic = 'upVcPE+wPOksRHztag2HLt+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0adoq8hudFeXNTakk0yR9Uf+j7SFVBtcMtEHiG7csiBBkxValpjT3ck=';
